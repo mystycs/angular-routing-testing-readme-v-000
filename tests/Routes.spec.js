f69720a8326b9cde13b8abe903474c884e4c156d
Mark <mystycs@gmail.com>
2016-11-16 14:56:05 -0500

    Done.

--- tests/Routes.spec.js
+++ tests/Routes.spec.js
@@ -1,19 +1,23 @@
 describe('Routes', function () {
-	var $state;
+    var $state;
 
-	beforeEach(module('app'));
+    beforeEach(module('app'));
 
-	beforeEach(inject(function ($injector) {
-		$state = $injector.get('$state');
-	}));
+    beforeEach(inject(function ($injector) {
+        $state = $injector.get('$state');
+    }));
 
-	describe('Settings', function () {
-		var state;
+    describe('Settings', function () {
+        var state;
 
-		it('should have the correct URL', function () {
-			state = $state.get('settings');
+        it('should have the correct URL', function () {
+            state = $state.get('settings');
 
-			expect(state.url).toEqual('/settings');
-		});
-	});
+            expect(state.url).toEqual('/settings');
+        });
+
+        it('should use the correct template', function () {
+            expect(state.templateUrl).toEqual('views/settings.html');
+        });
+    });
 });
